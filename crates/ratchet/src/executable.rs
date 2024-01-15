7f1a947f7390e9e008fca7a5f10d2b4876adab97
FL33TW00D <chris@fleetwood.dev>
2024-01-15 16:23:07 +0000

    chore: slow

--- crates/ratchet/src/executable.rs
+++ crates/ratchet/src/executable.rs
@@ -21,46 +21,11 @@ impl Executable {
                     cpass.set_bind_group(index as u32, bind_group, &[]);
                 }
                 let uniform_index = step.storage_groups().len() as u32;
-                cpass.set_bind_group(uniform_index, &self.uniform_group, &[step.offset()]);
-                let (x_count, y_count, z_count) = step.workload().get_counts();
+                cpass.set_bind_group(uniform_index, uniform_group, &[step.offset()]);
+                let [x_count, y_count, z_count] = step.workgroup_count().as_slice();
                 cpass.dispatch_workgroups(x_count, y_count, z_count);
             }
         }
-        self.handle.queue().submit(Some(encoder.finish()))
-    }
-
-    #[cfg(feature = "gpu-profiling")]
-    fn dispatch_operations(&self) -> SubmissionIndex {
-        let mut encoder = self
-            .handle
-            .device()
-            .create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
-
-        let mut profiler = Profiler::new(self.handle.clone(), self.steps.len() as _);
-
-        {
-            for op in self.steps.iter() {
-                let timestamp_writes =
-                    Some(profiler.create_timestamp_queries(op.node_id(), op.key()));
-
-                let mut cpass = encoder.begin_compute_pass(&wgpu::ComputePassDescriptor {
-                    label: None,
-                    timestamp_writes,
-                });
-                cpass.set_pipeline(op.pipeline());
-                for (index, bind_group) in op.storage_groups().iter().enumerate() {
-                    cpass.set_bind_group(index as u32, bind_group, &[]);
-                }
-                let uniform_index = op.storage_groups().len() as u32;
-                cpass.set_bind_group(uniform_index, &self.uniform_group, &[op.offset()]);
-                let (x_count, y_count, z_count) = op.workload().get_counts();
-                cpass.dispatch_workgroups(x_count, y_count, z_count);
-            }
-        }
-
-        profiler.resolve(&mut encoder);
-        let index = self.handle.queue().submit(Some(encoder.finish()));
-        profiler.read_timestamps(true);
-        index
+        device.queue().submit(Some(encoder.finish()))
     }
 }
