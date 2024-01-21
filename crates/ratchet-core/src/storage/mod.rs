15c38933c6c558f54161ceda2b38cad4a6f75958
FL33TW00D <chris@fleetwood.dev>
2024-01-21 11:46:57 +0000

    msg

--- crates/ratchet-core/src/storage/mod.rs
+++ crates/ratchet-core/src/storage/mod.rs
@@ -5,106 +5,58 @@ use bytemuck::NoUninit;
 pub use cpu_buffer::*;
 pub use gpu_buffer::*;
 
-use crate::{gpu::GPUBuffer, Device, DeviceError, Shape};
+use crate::{Device, DeviceError, Shape};
 
 use crate::DType;
 
 #[derive(Debug)]
-pub struct Storage {
-    raw: Option<RawStorage>, //Optional as the tensor may not be resolved
+pub enum Storage {
+    CPU(CPUBuffer),
+    GPU(GPUBuffer),
 }
-
-unsafe impl Send for Storage {}
-unsafe impl Sync for Storage {}
 
 impl Storage {
-    pub fn empty() -> Self {
-        Self { raw: None }
-    }
-
     pub fn from_slice<T: NoUninit>(data: &[T], shape: &Shape, device: &Device) -> Self {
-        assert_eq!(data.len(), shape.numel());
         match device {
-            Device::CPU => Self {
-                raw: Some(RawStorage::CPU(RawCPUBuffer::from_slice(data, shape))),
-            },
-            Device::GPU(d) => Self {
-                raw: Some(RawStorage::GPU(RawGPUBuffer::from_slice(data, shape, d))),
-            },
+            Device::CPU => Storage::CPU(CPUBuffer::from_slice(data, shape)),
+            Device::GPU(g) => Storage::GPU(GPUBuffer::from_slice(data, shape, g)),
         }
     }
 
-    pub fn set_raw(&mut self, raw: RawStorage) {
-        self.raw = Some(raw);
-    }
-
-    pub fn raw(&self) -> Option<&RawStorage> {
-        self.raw.as_ref()
-    }
-
-    pub fn try_gpu(&self) -> Result<&RawGPUBuffer, DeviceError> {
-        match self.raw.as_ref() {
-            Some(RawStorage::GPU(raw)) => Ok(raw),
-            _ => Err(DeviceError::DeviceMismatch(
-                "GPU".to_string(),
-                "CPU".to_string(),
-            )),
+    pub fn dump(&self, dt: DType, full: bool) -> String {
+        match self {
+            Storage::CPU(c) => c.dump(dt, full),
+            Storage::GPU(g) => g.dump(dt, full),
         }
     }
 
-    pub fn dump(&self, dtype: DType, full: bool) -> String {
-        self.raw
-            .as_ref()
-            .map(|raw| match raw {
-                RawStorage::CPU(raw) => raw.dump(dtype, full),
-                RawStorage::GPU(raw) => raw.dump(dtype, full),
-            })
-            .unwrap_or_else(|| "None".to_string())
-    }
-}
-
-impl From<RawStorage> for Storage {
-    fn from(raw: RawStorage) -> Self {
-        Self { raw: Some(raw) }
-    }
-}
-
-impl From<RawCPUBuffer> for Storage {
-    fn from(raw: RawCPUBuffer) -> Self {
-        Self {
-            raw: Some(RawStorage::CPU(raw)),
+    pub fn try_cpu(&self) -> Result<&CPUBuffer, DeviceError> {
+        match self {
+            Storage::CPU(c) => Ok(c),
+            _ => unimplemented!(),
         }
     }
-}
 
-impl From<RawGPUBuffer> for Storage {
-    fn from(raw: RawGPUBuffer) -> Self {
-        Self {
-            raw: Some(RawStorage::GPU(raw)),
+    pub fn try_gpu(&self) -> Result<&GPUBuffer, DeviceError> {
+        match self {
+            Storage::GPU(g) => Ok(g),
+            _ => unimplemented!(),
         }
     }
-}
 
-#[derive(Debug)]
-pub enum RawStorage {
-    CPU(RawCPUBuffer),
-    GPU(RawGPUBuffer),
-}
-
-impl RawStorage {
-    pub fn from_gpu(buf: GPUBuffer, dtype: DType) -> Self {
-        RawStorage::GPU(RawGPUBuffer {
-            inner: buf,
-            alignment: dtype.size_of(),
-        })
+    pub fn deep_clone(&self) -> Result<Self, DeviceError> {
+        match self {
+            Storage::CPU(c) => Ok(Storage::CPU(c.deep_clone()?)),
+            _ => todo!(),
+        }
     }
 }
 
 pub trait DeviceStorage: std::fmt::Debug + Clone + 'static {
     // To be expanded to other devices
-    fn to_device(self, device: &Device) -> Result<RawGPUBuffer, DeviceError>;
+    fn to_device(&self, device: &Device) -> Result<GPUBuffer, DeviceError>;
     /// Creates a copy of the device buffer on the CPU
-    fn to_cpu(&self, device: &Device) -> Result<RawCPUBuffer, DeviceError>;
+    fn to_cpu(&self, device: &Device) -> Result<CPUBuffer, DeviceError>;
     fn n_bytes(&self) -> usize;
     fn dump(&self, dt: DType, full: bool) -> String;
 }
