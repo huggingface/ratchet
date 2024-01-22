aa651c2bfe71b96908fbd3fa79763ca251ad37ca
FL33TW00D <chris@fleetwood.dev>
2024-01-22 16:21:38 +0000

    chore: no idea

--- crates/ratchet-core/src/tensor.rs
+++ crates/ratchet-core/src/tensor.rs
@@ -415,6 +415,7 @@ mod tests {
 
     use super::*;
 
+    /*
     #[test]
     fn dbg() -> anyhow::Result<()> {
         let cpu_device = Device::request_device(DeviceRequest::CPU)?;
@@ -434,49 +435,48 @@ mod tests {
         println!("{:?}", b_cpu);
         Ok(())
     }
-
-    /*
-        #[test]
-        fn test_pyo3() -> anyhow::Result<()> {
-            let cpu_device = Device::request_device(DeviceRequest::CPU)?;
-            let a = Tensor::randn::<f32>(shape![1024, 1024], cpu_device.clone());
-            let b = Tensor::randn::<f32>(shape![1024, 1024], cpu_device.clone());
-
-            /*
-                    let ground: anyhow::Result<Tensor> = Python::with_gil(|py| {
-                        let prg = PyModule::from_code(
-                            py,
-                            r#"
-            import torch
-
-            def matmul(a, b):
-                return torch.matmul(torch.from_numpy(a), torch.from_numpy(b)).numpy()
-                                    "#,
-                            "x.py",
-                            "x",
-                        )?;
-
-                        let py_a = a.to_py::<f32>(&py);
-                        println!("py_a: {:?}", py_a);
-                        let py_b = b.to_py::<f32>(&py);
-                        println!("py_b: {:?}", py_b);
-
-                        let py_c = prg
-                            .getattr("matmul")?
-                            .call1((py_a, py_b))?
-                            .extract::<&PyArrayDyn<f32>>()?;
-                        println!("py_c: {:?}", py_c);
-                        Ok(Tensor::from(py_c))
-                    });
-                    */
-            let device = Device::request_device(DeviceRequest::GPU)?;
-            let a_gpu = a.to(device.clone())?;
-            let b_gpu = b.to(device.clone())?;
-            let c_gpu = a_gpu.matmul(&b_gpu)?;
-            c_gpu.resolve()?;
-            let d_gpu = c_gpu.to(Device::CPU)?;
-            println!("Ours: {:?}", d_gpu);
-            Ok(())
-        }
     */
+
+    #[test]
+    fn test_pyo3() -> anyhow::Result<()> {
+        let cpu_device = Device::request_device(DeviceRequest::CPU)?;
+        let a = Tensor::randn::<f32>(shape![1024, 1024], cpu_device.clone());
+        let b = Tensor::randn::<f32>(shape![1024, 1024], cpu_device.clone());
+
+        /*
+                let ground: anyhow::Result<Tensor> = Python::with_gil(|py| {
+                    let prg = PyModule::from_code(
+                        py,
+                        r#"
+        import torch
+
+        def matmul(a, b):
+            return torch.matmul(torch.from_numpy(a), torch.from_numpy(b)).numpy()
+                                "#,
+                        "x.py",
+                        "x",
+                    )?;
+
+                    let py_a = a.to_py::<f32>(&py);
+                    println!("py_a: {:?}", py_a);
+                    let py_b = b.to_py::<f32>(&py);
+                    println!("py_b: {:?}", py_b);
+
+                    let py_c = prg
+                        .getattr("matmul")?
+                        .call1((py_a, py_b))?
+                        .extract::<&PyArrayDyn<f32>>()?;
+                    println!("py_c: {:?}", py_c);
+                    Ok(Tensor::from(py_c))
+                });
+                */
+        let device = Device::request_device(DeviceRequest::GPU)?;
+        let a_gpu = a.to(device.clone())?;
+        let b_gpu = b.to(device.clone())?;
+        let c_gpu = a_gpu.matmul(&b_gpu)?;
+        c_gpu.resolve()?;
+        let d_gpu = c_gpu.to(Device::CPU)?;
+        println!("Ours: {:?}", d_gpu);
+        Ok(())
+    }
 }
