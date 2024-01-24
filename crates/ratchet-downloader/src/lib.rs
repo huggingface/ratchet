b164c33ea358490921fde4ff052a745b79895eac
sigma-andex <77549848+sigma-andex@users.noreply.github.com>
2024-01-24 14:53:41 +0000

    Start with header parsing

--- crates/ratchet-downloader/src/lib.rs
+++ crates/ratchet-downloader/src/lib.rs
@@ -10,6 +10,7 @@ use wasm_bindgen_futures::JsFuture;
 use wasm_streams::ReadableStream;
 use web_sys::{console, ReadableStreamGetReaderOptions, ReadableStreamReaderMode};
 use winnow::{binary::bits::bytes, prelude::*, stream::Stream, Bytes, Partial};
+use winnow::{binary::u32, binary::u64, combinator::preceded, Parser};
 
 mod fetch;
 pub mod huggingface;
@@ -22,7 +23,7 @@ pub fn js_error(message: String) -> JsError {
     JsError::new(message.as_str())
 }
 
-type GGUFStream<'i> = Partial<&'i Bytes>;
+type BytesStream<'i> = Partial<&'i Bytes>;
 
 pub struct Model {
     url: String,
@@ -60,7 +61,7 @@ impl Model {
         let mut buf = [0u8; 100];
         let result = async_read.read_exact(&mut buf).await?;
 
-        let mut test = GGUFStream::new(Bytes::new(&buf));
+        let mut test = BytesStream::new(Bytes::new(&buf));
 
         let g1 = &test.next_token();
         let g2 = &test.next_token();
@@ -68,6 +69,102 @@ impl Model {
         let f = &test.next_token();
         debug!("Done!:", format!("{:?}{:?}{:?}{:?}", g1, g2, u, f));
 
+        Ok(())
+    }
+}
+
+mod gguf {
+    use crate::BytesStream;
+    use winnow::binary::u32;
+    use winnow::binary::u64;
+    use winnow::binary::Endianness;
+    use winnow::Parser;
+
+    #[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
+    pub struct Header {
+        pub version: u32,
+        pub tensor_count: u64,
+        pub metadata_kv_count: u64,
+    }
+
+    #[inline]
+    fn magic_number(input: &mut BytesStream) -> winnow::PResult<()> {
+        // [TODO] Fix endianness
+        (71, 71, 85, 70).parse_next(input).map(|_magic_number| ())
+    }
+
+    #[inline]
+    fn version(input: &mut BytesStream) -> winnow::PResult<u32> {
+        u32(Endianness::Little).parse_next(input)
+    }
+
+    #[inline]
+    fn tensor_count(input: &mut BytesStream) -> winnow::PResult<u64> {
+        u64(Endianness::Little).parse_next(input)
+    }
+
+    #[inline]
+    fn metadata_kv_count(input: &mut BytesStream) -> winnow::PResult<u64> {
+        u64(Endianness::Little).parse_next(input)
+    }
+
+    #[inline]
+    fn metadata_kv(input: &mut BytesStream) -> winnow::PResult<u64> {
+        u64(Endianness::Little).parse_next(input)
+    }
+
+    pub fn parse_header(input: &mut BytesStream) -> winnow::PResult<Header> {
+        (magic_number, version, tensor_count, metadata_kv_count)
+            .parse_next(input)
+            .map(|(gguf, version, tensor_count, metadata_kv_count)| Header {
+                version,
+                tensor_count,
+                metadata_kv_count,
+            })
+    }
+}
+
+pub fn to_std_error(error: winnow::error::ErrMode<winnow::error::ContextError>) -> std::io::Error {
+    match error {
+        winnow::error::ErrMode::Backtrack(err) => {
+            std::io::Error::new(std::io::ErrorKind::Other, "Backtrack")
+        }
+        winnow::error::ErrMode::Cut(err) => std::io::Error::new(std::io::ErrorKind::Other, "Cut"),
+        winnow::error::ErrMode::Incomplete(needed) => {
+            std::io::Error::new(std::io::ErrorKind::Other, "Needed")
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::io::Read;
+
+    use anyhow::Error;
+    use winnow::Bytes;
+
+    use crate::{gguf, to_std_error, BytesStream};
+
+    #[test]
+    fn test_parse_header() -> anyhow::Result<()> {
+        let mut file = std::fs::File::open("./test-data/TheBloke_TinyLlama-1.1B-Chat-v1.0-GGUF/tinyllama-1.1b-chat-v1.0.Q4_K_M.gguf")?;
+
+        let buffer_size = 30;
+        let min_buffer_growth = 100;
+        let buffer_growth_factor = 2;
+        let mut buffer = circular::Buffer::with_capacity(buffer_size);
+        let read = file.read(buffer.space())?;
+        buffer.fill(read);
+
+        let mut input = BytesStream::new(Bytes::new(buffer.data()));
+
+        let result = gguf::parse_header(&mut input).map_err(to_std_error)?;
+        let expected = gguf::Header {
+            version: 3,
+            tensor_count: 201,
+            metadata_kv_count: 23,
+        };
+        assert_eq!(result, expected);
         Ok(())
     }
 }
