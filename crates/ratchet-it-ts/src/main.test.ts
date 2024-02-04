8b004d7860c86670d581de6a703c0a73439a4399
sigma-andex <77549848+sigma-andex@users.noreply.github.com>
2024-02-04 12:02:46 +0000

    Add caching

--- crates/ratchet-it-ts/src/main.test.ts
+++ crates/ratchet-it-ts/src/main.test.ts
@@ -9,9 +9,23 @@ beforeAll(async () => {
 describe("The ApiBuilder", () => {
   it("should download a model from HF hub", async () => {
     const api: Api = ApiBuilder.from_hf(`jantxu/ratchet-test`).build();
-    const modelStream = await api.get("model.safetensors");
 
-    const bytes = await modelStream.bytes();
+    console.log(`Getting model first time.`);
+    const modelStream = await api.get("model.safetensors");
+    const bytes = await modelStream.to_uint8();
     expect(bytes.length).toBe(8388776);
+
+    const cached = modelStream.is_cached();
+    console.log(`Cached ${cached}.`);
+    expect(cached).toBe(false);
+
+    console.log(`Getting model second time.`);
+    const modelStream2 = await api.get("model.safetensors");
+    const bytes2 = await modelStream2.to_uint8();
+    expect(bytes2.length).toBe(8388776);
+
+    const cached2 = modelStream2.is_cached();
+    console.log(`Cached ${cached2}.`);
+    expect(cached2).toBe(true);
   });
 });
