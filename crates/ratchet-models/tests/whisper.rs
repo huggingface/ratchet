c4d26c45c00efe14d83d2c7bb810f4f1767eebba
FL33TW00D <chris@fleetwood.dev>
2024-02-10 11:06:48 +0000

    chore: trying wasm bindgen test for whisper encoder

---
+++ crates/ratchet-models/tests/whisper.rs
@@ -0,0 +1,37 @@
+use ratchet::{shape, Device, DeviceRequest, Tensor};
+use ratchet_client::ApiBuilder;
+use ratchet_loader::GGMLCompatible;
+use ratchet_models::{Whisper, WhisperEncoder};
+use ratchet_nn::Module;
+use wasm_bindgen::prelude::*;
+use wasm_bindgen_test::wasm_bindgen_test;
+
+wasm_bindgen_test::wasm_bindgen_test_configure!(run_in_browser);
+
+fn log_init() {
+    console_error_panic_hook::set_once();
+    log::set_max_level(log::LevelFilter::Off);
+    console_log::init_with_level(log::Level::Warn).unwrap();
+}
+
+#[wasm_bindgen_test]
+async fn chrome_tiny_encoder() -> Result<(), JsValue> {
+    log_init();
+    let model_repo = ApiBuilder::from_hf("ggerganov/whisper.cpp").build();
+    let model = model_repo.get("ggml-tiny.bin").await?;
+    let uint8array = model.to_uint8().await?;
+
+    let mut reader = std::io::BufReader::new(std::io::Cursor::new(uint8array.to_vec()));
+    let gg = Whisper::load_ggml(&mut reader).unwrap();
+
+    let device = Device::request_device(DeviceRequest::GPU).unwrap();
+    let encoder = WhisperEncoder::load(&gg, &mut reader, &device).unwrap();
+    let input = Tensor::randn::<f32>(shape![1, 80, 3000], device);
+
+    let result = encoder.forward(&input).unwrap();
+    result.resolve().unwrap();
+    let ours = result.to(&Device::CPU).unwrap();
+    println!("OURS: {:#?}", ours);
+
+    Ok(())
+}
