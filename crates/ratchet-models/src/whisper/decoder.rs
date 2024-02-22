3ae0cddf86fb3905b7c4be4f0a418541caea1573
FL33TW00D <chris@fleetwood.dev>
2024-02-22 11:41:07 +0000

    chore: fix test

--- crates/ratchet-models/src/whisper/decoder.rs
+++ crates/ratchet-models/src/whisper/decoder.rs
@@ -195,6 +195,7 @@ def ground(options):
         let dataset = api.dataset("FL33TW00D-HF/ratchet-util".to_string());
         let options = DecodingOptionsBuilder::new().build();
         let hs_npy = load_npy(dataset.get("jfk_tiny_encoder_hs.npy").unwrap());
+        let audio_path = dataset.get("jfk.wav").unwrap();
 
         let mut reader = std::io::BufReader::new(std::fs::File::open(path).unwrap());
         let gg_disk = Whisper::load_ggml(&mut reader).unwrap();
