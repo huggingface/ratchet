635c9a026cb4d02658079774ab9e1d02c55cd543
sigma-andex <77549848+sigma-andex@users.noreply.github.com>
2024-03-06 15:20:54 +0000

    Add test setup

--- crates/ratchet-loader/src/gguf/mod.rs
+++ crates/ratchet-loader/src/gguf/mod.rs
@@ -2,3 +2,51 @@ pub mod ggml;
 pub mod gguf;
 pub mod k_quants;
 pub mod utils;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use ratchet::{Device, DeviceRequest};
+
+    #[tokio::test]
+    async fn test_gguf() -> anyhow::Result<()> {
+        const DUMMY_GGUF: &[u8] =
+            include_bytes!(concat!(env!("CARGO_MANIFEST_DIR"), "/test-data/TheBloke_TinyLlama-1.1B-Chat-v1.0-GGUF/tinyllama-1.1b-chat-v1.0.Q4_K_M.gguf"));
+
+        let mut reader = std::io::BufReader::new(std::io::Cursor::new(DUMMY_GGUF.to_vec()));
+
+        let result = gguf::Content::read(&mut reader)?;
+
+        let empty_value = gguf::Value::String(String::from(""));
+        let metadata = result
+            .metadata
+            .keys()
+            .filter(|key| !(*key).starts_with("tokenizer"))
+            .map(|key| {
+                (
+                    key,
+                    result.metadata.get(key).unwrap_or_else(|| &empty_value),
+                )
+            })
+            .collect::<Vec<_>>();
+
+        println!("{:?}", metadata);
+
+        println!(
+            "{:?}",
+            result
+                .tensor_infos
+                .keys()
+                .filter(|key| (*key).starts_with("blk.0"))
+                .collect::<Vec<_>>()
+        );
+
+        let device = Device::request_device(DeviceRequest::GPU)?;
+        // let tensor = result.tensor(&mut reader, "blk.0.ffn_norm.weight", &device);
+        let tensor = result.tensor(&mut reader, "blk.0.attn_k.weight", &device);
+        // let model_data = file.gguf().await?;
+
+        println!("{:?}", tensor);
+        Ok(())
+    }
+}
