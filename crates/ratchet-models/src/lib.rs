593fb086338d263abecb75abd94985ea766e6fd8
FL33TW00D <chris@fleetwood.dev>
2024-03-21 09:53:08 +0000

    chore: experimenting with model registry

--- crates/ratchet-models/src/lib.rs
+++ crates/ratchet-models/src/lib.rs
@@ -1,3 +1,41 @@
 mod whisper;
 
 pub use whisper::*;
+
+use std::collections::HashMap;
+
+pub enum WhisperVariants {
+    Tiny,
+    Small,
+    Large,
+}
+
+pub enum LlamaVariants {
+    _7B,
+    _13B,
+}
+
+pub enum AvailableModels {
+    Whisper(WhisperVariants),
+    Llama(LlamaVariants),
+}
+
+/// # ModelID
+///
+/// Unique identifier for a model on the Hub, e.g `FL33TW00D-HF/whisper-tiny`
+pub struct ModelID {
+    repo: String,
+    model_name: String,
+}
+
+pub struct RegisteredModel {
+    pub variant: String,
+    pub key: ModelID,
+}
+
+// # Model Registry
+//
+// The model registry maps from user-facing AvailableModels to a ModelCollection.
+// A ModelCollection contains all of the variants of the specified model e.g (TINY, SMALL, LARGE)
+// or (7B, 13B).
+pub struct ModelRegistry(HashMap<AvailableModels, Vec<RegisteredModel>>);
