cb4382aa64c95a3352eb6d171f07adcb24492ca1
FL33TW00D <chris@fleetwood.dev>
2024-03-25 10:55:11 +0000

    chore: extraction

--- crates/ratchet-models/src/phi2/model.rs
+++ crates/ratchet-models/src/phi2/model.rs
@@ -1,3 +1,7 @@
+pub struct Phi2 {}
+
+impl Phi2 {}
+
 #[cfg(all(test, not(target_arch = "wasm32")))]
 mod tests {
     use numpy::PyArrayDyn;
@@ -8,9 +12,12 @@ mod tests {
         let prg = r#"
 import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer
+import collections
 
 def ground():
+    extracted = collections.defaultdict(list)
     model = AutoModelForCausalLM.from_pretrained("microsoft/phi-2", torch_dtype=torch.float32, device_map="cpu", trust_remote_code=True)
+    print(model)
     tokenizer = AutoTokenizer.from_pretrained("microsoft/phi-2", trust_remote_code=True)
 
     inputs = tokenizer('''def print_prime(n):
@@ -18,9 +25,13 @@ def ground():
     Print all primes between 1 and n
     """''', return_tensors="pt", return_attention_mask=False)
 
-    outputs = model.generate(**inputs, max_length=200, return_dict_in_generate=True, output_logits=True)
-    logits = list(outputs["logits"])
-    return [l.detach().numpy() for l in logits]
+    model.model.embed_tokens.register_forward_hook(lambda module, inputs, outputs: extracted["embed_tokens"].append(outputs))
+    outputs = model.generate(**inputs, max_length=24, return_dict_in_generate=True, output_logits=True)
+    print("Extracted", extracted)
+    print("First shape", extracted["embed_tokens"][0].shape)
+    
+    #logits = list(outputs["logits"])
+    #return [l.detach().numpy() for l in logits]
 "#;
         Python::with_gil(|py| {
             let prg = PyModule::from_code(py, &prg, "x.py", "x")?;
