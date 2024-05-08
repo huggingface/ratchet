b0ea25f91e33155a3a249f10aa1c273fdeea71b0
FL33TW00D <chris@fleetwood.dev>
2024-05-08 15:11:59 +0100

    chore: add raw mel

--- crates/ratchet-models/src/whisper/transcribe.rs
+++ crates/ratchet-models/src/whisper/transcribe.rs
@@ -169,3 +169,90 @@ pub async fn transcribe(
     t.generate_formatted(&tokenizer);
     Ok(t)
 }
+
+#[cfg(target_arch = "wasm32")]
+pub async fn transcribe_raw(
+    model: &mut Whisper,
+    mel: Vec<f32>,
+    mut decode_options: DecodingOptions,
+    callback: Option<impl Fn(StreamedSegment)>,
+) -> anyhow::Result<TranscriptionResult> {
+    let runtime = Instant::now();
+    let n_mels = model.config.n_mels as usize;
+    let content_frames = mel.shape()[mel.rank() - 1] - N_FRAMES;
+
+    if decode_options.language.is_none() {
+        if !model.is_multilingual() {
+            log::warn!("No language specified, using English");
+            decode_options.language = Some(Language::String("en".to_string()));
+        } else {
+            log::warn!("No language specified, using language detection");
+            let mel = mel.clone().slice(&[0..1, 0..n_mels, 0..3000])?;
+            decode_options.language = Some(model.detect_language(mel).await?);
+        }
+    }
+
+    let language = decode_options.language.as_ref().unwrap();
+    let task = decode_options.task;
+    let tokenizer =
+        WhisperTokenizer::load(None, n_mels == 128, language.clone(), task.clone()).await;
+
+    let mut seek = 0;
+    let input_stride = N_FRAMES / N_AUDIO_CTX;
+    let mut all_tokens = Vec::with_capacity(512);
+    let mut all_segments = Vec::with_capacity(512);
+    let prompt_since_reset = 0;
+
+    while seek < content_frames {
+        let mut decode_options = decode_options.clone();
+        let time_offset = (seek * HOP_LENGTH) as f64 / SAMPLE_RATE as f64;
+        decode_options.time_offset = Some(time_offset);
+        let mel_segment = mel
+            .clone()
+            .slice(&[0..1, 0..n_mels, seek..(seek + N_FRAMES)])?;
+        log::info!("Processing segment: {} -> {}", seek, seek + N_FRAMES);
+
+        let segment_size = min(N_FRAMES, content_frames - seek);
+        let segment_duration = segment_size * HOP_LENGTH / SAMPLE_RATE;
+
+        if !all_tokens.is_empty() {
+            decode_options.prompt = Some(Prompt::Tokens(all_tokens[prompt_since_reset..].to_vec()));
+        }
+
+        let hs = model.encoder.schedule(mel_segment)?.resolve()?;
+
+        let task = DecodingTask::new(decode_options, tokenizer.clone());
+        let decoded = task.run(&mut model.decoder, hs, &callback).await?;
+
+        let (segments, advance) = DecodingTask::build_segments(
+            &tokenizer,
+            decoded,
+            time_offset,
+            segment_size,
+            segment_duration,
+            input_stride,
+        );
+        let all_segment_tokens = segments
+            .iter()
+            .flat_map(|s| s.tokens.iter().copied())
+            .map(|x| x as i32)
+            .collect::<Vec<_>>();
+        all_tokens.extend(all_segment_tokens);
+        all_segments.extend(segments);
+        model.decoder.reset();
+        seek += advance;
+    }
+
+    if let Some(cb) = callback {
+        cb(StreamedSegment::from_tokens(
+            &tokenizer,
+            &[WhisperTokenizer::EOT],
+            content_frames as f64 / 100.,
+            true,
+        ));
+    }
+
+    let mut t = TranscriptionResult::new(runtime.elapsed(), all_segments, None);
+    t.generate_formatted(&tokenizer);
+    Ok(t)
+}
