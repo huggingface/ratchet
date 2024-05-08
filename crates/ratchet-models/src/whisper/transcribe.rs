b9094d971538f87079f1ba0a38d3909e49265ddf
FL33TW00D <chris@fleetwood.dev>
2024-05-08 15:27:29 +0100

    chore: funky

--- crates/ratchet-models/src/whisper/transcribe.rs
+++ crates/ratchet-models/src/whisper/transcribe.rs
@@ -82,16 +82,18 @@ pub fn transcribe(
     Ok(t)
 }
 
-#[cfg(target_arch = "wasm32")]
-pub async fn transcribe(
+#[cfg(not(target_arch = "wasm32"))]
+pub fn transcribe_raw(
     model: &mut Whisper,
-    audio: Vec<f32>,
+    mel: Vec<f32>,
     mut decode_options: DecodingOptions,
     callback: Option<impl Fn(StreamedSegment)>,
 ) -> anyhow::Result<TranscriptionResult> {
+    use ratchet::{shape, Tensor};
+
+    let n_mels = model.config.n_mels;
     let runtime = Instant::now();
-    let n_mels = model.config.n_mels as usize;
-    let mel = model.specgen.generate(audio)?.to(&model.device).await?;
+    let mel = Tensor::from_data(&mel, shape![1, 80, mel.len() / 80], model.device.clone());
     let content_frames = mel.shape()[mel.rank() - 1] - N_FRAMES;
 
     if decode_options.language.is_none() {
@@ -101,14 +103,13 @@ pub async fn transcribe(
         } else {
             log::warn!("No language specified, using language detection");
             let mel = mel.clone().slice(&[0..1, 0..n_mels, 0..3000])?;
-            decode_options.language = Some(model.detect_language(mel).await?);
+            decode_options.language = Some(model.detect_language(mel)?);
         }
     }
 
     let language = decode_options.language.as_ref().unwrap();
     let task = decode_options.task;
-    let tokenizer =
-        WhisperTokenizer::load(None, n_mels == 128, language.clone(), task.clone()).await;
+    let tokenizer = WhisperTokenizer::load(None, n_mels == 128, language.clone(), task);
 
     let mut seek = 0;
     let input_stride = N_FRAMES / N_AUDIO_CTX;
@@ -135,8 +136,7 @@ pub async fn transcribe(
         let hs = model.encoder.schedule(mel_segment)?.resolve()?;
 
         let task = DecodingTask::new(decode_options, tokenizer.clone());
-        let decoded = task.run(&mut model.decoder, hs, &callback).await?;
-
+        let decoded = task.run(&mut model.decoder, hs, &callback)?;
         let (segments, advance) = DecodingTask::build_segments(
             &tokenizer,
             decoded,
@@ -156,29 +156,21 @@ pub async fn transcribe(
         seek += advance;
     }
 
-    if let Some(cb) = callback {
-        cb(StreamedSegment::from_tokens(
-            &tokenizer,
-            &[WhisperTokenizer::EOT],
-            content_frames as f64 / 100.,
-            true,
-        ));
-    }
-
     let mut t = TranscriptionResult::new(runtime.elapsed(), all_segments, None);
     t.generate_formatted(&tokenizer);
     Ok(t)
 }
 
 #[cfg(target_arch = "wasm32")]
-pub async fn transcribe_raw(
+pub async fn transcribe(
     model: &mut Whisper,
-    mel: Vec<f32>,
+    audio: Vec<f32>,
     mut decode_options: DecodingOptions,
     callback: Option<impl Fn(StreamedSegment)>,
 ) -> anyhow::Result<TranscriptionResult> {
     let runtime = Instant::now();
     let n_mels = model.config.n_mels as usize;
+    let mel = model.specgen.generate(audio)?.to(&model.device).await?;
     let content_frames = mel.shape()[mel.rank() - 1] - N_FRAMES;
 
     if decode_options.language.is_none() {
