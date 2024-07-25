09bc5fa09ec3b2e9c1f24b296bf7a96bf1a8ebd6
FL33TW00D <chris@fleetwood.dev>
2024-07-25 13:54:03 +0100

    chore: generate test case

--- crates/ratchet-validator/src/main.rs
+++ crates/ratchet-validator/src/main.rs
@@ -1,7 +1,47 @@
+use serde::Serialize;
 use tch::Tensor;
 
-fn main() {
-    let t = Tensor::from_slice(&[3, 1, 4, 1, 5]);
-    let t = t * 2;
-    t.print();
+#[derive(Serialize)]
+struct TestCase {
+    name: String,
+    op: String,
+    inputs: Vec<f32>,
+    outputs: Vec<f32>,
 }
+
+fn generate_input_pair<S: torch_sys::IntList>(shape: S) -> (Tensor, Vec<f32>) {
+    let input = Tensor::f_randn(shape, (tch::Kind::Float, tch::Device::Cpu)).unwrap();
+    let mut copy = Tensor::empty_like(&input);
+    copy.copy_(&input);
+    let inputv: Vec<f32> = Vec::try_from(copy.reshape(-1)).unwrap();
+    (input, inputv)
+}
+
+// If you have random inputs
+// Then CI may randomly fail when you generate an input that finds a bug
+fn main() {
+    let (input, inputv) = generate_input_pair(&[128, 128]);
+    let output: Vec<f32> = Vec::try_from(input.abs().reshape(-1)).unwrap();
+    let test_case = TestCase {
+        name: "abs-f32".to_string(),
+        op: "abs".to_string(),
+        inputs: inputv,
+        outputs: output,
+    };
+    let json = serde_json::to_string(&test_case).unwrap();
+    println!("{}", json);
+}
+
+//Run the test suite, and generate the JSON locally
+//
+//    {
+//       "cases": [
+//            {
+//                name: "abs-f16",
+//                op: "abs",
+//                "inputs": [.......],
+//                "outputs": [.......]
+//            }
+//        ]
+//    }
+//golden_truth.json
